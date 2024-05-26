90a60bae19f54c4a53f6241c49d6f2be905ce9ed
Mia O'Brien <mihribanguneydas@gmail.com>
2024-05-26 17:00:38 -0600

    Time: 44 ms (80.48%), Space: 16.9 MB (63.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if(sorted(s)== sorted(t)):
+        if(Counter(s)== Counter(t)): #if(sorted(s)== sorted(t)): Counter is fasterthan sorted
             return True
         else:
             return False
